bb271975d4a5a32fae964e45c787651663f7320b
Starinin Andrey <starininandrey@gmail.com>
2022-07-03 11:17:38 +0300

    Add return result from task

---
+++ TaskDemo.App4/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace TaskDemo.App4
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            const int NUMBER = 15;
+            Console.WriteLine("Основная программа запущена");
+            
+            var task = new Task<int>(() => Factorial(NUMBER));
+            task.Start();
+            var factorial = task.Result;
+            Console.WriteLine($"{NUMBER}! = {factorial}");
+
+            Console.WriteLine("Основная программа завершена");
+
+            Console.ReadKey();
+        }
+
+        private static int Factorial(int number)
+        {
+            int res = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                res *= i;
+                Console.WriteLine($"{i}! = {res}");
+            }
+            return res;
+        }
+    }
+}
