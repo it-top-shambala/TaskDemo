2049ffc501562a57c97d89d65d9de05364d678a3
Starinin Andrey <starininandrey@gmail.com>
2022-07-03 11:28:27 +0300

    Add continuation task

---
+++ TaskDemo.App5/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace TaskDemo.App5
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Основная программа запущена");
+
+            var task = new Task<int>(() => TaskExample(5));
+            var task2 = task.ContinueWith(PrintResult);
+            
+            task.Start();
+            task2.Wait();
+
+            Console.WriteLine("Основная программа завершена");
+
+            Console.ReadKey();
+        }
+
+        private static int TaskExample(int number)
+        {
+            return number * number;
+        }
+
+        private static void PrintResult(Task<int> task)
+        {
+            Console.WriteLine(task.Result);
+        }
+    }
+}
